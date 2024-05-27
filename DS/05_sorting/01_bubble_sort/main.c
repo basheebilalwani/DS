9a719efe8289a8720fabf30d87f0fa8ab4b5ca93
Bashee <164609388+basheebilalwani@users.noreply.github.com>
2024-05-27 22:45:28 +0530

    Update main.c

--- DS/05_sorting/01_bubble_sort/main.c
+++ DS/05_sorting/01_bubble_sort/main.c
@@ -5,20 +5,20 @@ void bubblesort(int arr[], int n)
 {
 	int i, j, temp, flag;
 	for(i=0; i< n-1; i++)
-	{		
+{		
 		for(j=0; j<n-1-i; j++)
-		{
-			flag=0;
-			if(arr[j]>arr[j+1])
-			{
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-				flag=1;
-			}
-		}
-		if(flag==0)
-			break;
+{
+	 flag=0;
+	 if(arr[j]>arr[j+1])
+	{
+		temp=arr[j];
+		arr[j]=arr[j+1];
+		arr[j+1]=temp;
+		flag=1;
+	}
+}
+	if(flag==0)
+		break;
 	}
 }
 
